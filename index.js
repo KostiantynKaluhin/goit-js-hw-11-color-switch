1bc863c6f043d411d8059c5b77e1a1354e05586e
Kostianyn <odessavshok@gmail.com>
2021-10-20 04:10:35 +0300

    End

--- index.js
+++ index.js
@@ -7,11 +7,34 @@ const colors = [
   '#795548',
 ];
 const bodyRef = document.querySelector('body');
-const buttonsDivRef = document.querySelector('#buttons');
+const buttonsContainerRef = document.querySelector('#buttons');
 const startButtonRef = document.querySelector('.js-start');
 const stopButtonRef = document.querySelector('.js-stop');
-buttonsDivRef.addEventListener('click', buttonsFunctions);
+buttonsContainerRef.addEventListener('click', buttonsFunctions);
 
 const randomIntegerFromInterval = (min, max) => {
   return Math.floor(Math.random() * (max - min + 1) + min);
 };
+
+let randomColor;
+function setNewColor() {
+  randomColor = colors[randomIntegerFromInterval(0, 5)];
+  bodyRef.removeAttribute('style');
+  bodyRef.setAttribute('style', `background-color: ${randomColor}`);
+}
+
+stopButtonRef.disabled = true;
+
+let colorInterval;
+
+function buttonsFunctions(event, randomColor) {
+  if (event.target.textContent === 'Start') {
+    startButtonRef.disabled = true;
+    stopButtonRef.disabled = false;
+    colorInterval = setInterval(setNewColor, 1000);
+  } else if (event.target.textContent === 'Stop') {
+    startButtonRef.disabled = false;
+    stopButtonRef.disabled = true;
+    clearInterval(colorInterval);
+  }
+}
